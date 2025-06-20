fb6f4cdd03330ef493d4ac83ada481e7816e6414
Aakash <aakashpawar496@gmail.com>
2025-06-20 09:51:29 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aakash Pawar',
+    author_email= 'aakashpawar496@gmail.com',
+    packages= find_packages(),
+    install_requries = []
+)
